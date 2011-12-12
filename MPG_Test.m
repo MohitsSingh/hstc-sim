631c94ff3e49e782e62486e7a43967ed4b2d4992
steven.studniarz <steven.studniarz@gmail.com>
2011-12-12 21:59:30 +0000

    Updates from class

--- MPG_Test.m
+++ MPG_Test.m
@@ -1,9 +1,17 @@
 %Can check against values from:
 %http://ecomodder.com/forum/tool-aero-rolling-resistance.php
 
+xlabel('Velocity (mph)');
+ylabel('Fuel economy (mpg)');
+hold on;
 for i=5:5:120
    v = Vehicle;
    v.velocity = i;
    CalculateMPG(v, -1);
+   mpg = v.fuelEconomy;
+   
+   scatter(v.velocity, mpg,3,[.5 0 0],'filled');
+   hold on;
+   
    disp([num2str(i), ' ', num2str(v.fuelEconomy)])
 end
